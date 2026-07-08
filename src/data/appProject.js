6f707b80c62180062b3a2cd42d8959be7a88984b
zee1013 <dmswl1251@gamil.com>
2026-07-08 16:51:48 +0900

    프로토타입 링크 변경

--- src/data/appProject.js
+++ src/data/appProject.js
@@ -27,7 +27,7 @@ export const appProjects = [
       "Planti는 사용자가 하루동안 느낀 감정과 루틴을 부담없이 기록할 수 있도록 돕는 감정 기록 앱이다. 기존 기록 서비스의 낮은 지속성과 기록 피로도 문제를 해결하고자 기획되었으며, 사용자가 자신의 감정과 일상을 자연스럽게 돌아보며 기록 습관을 형성할 수 있는 경험을 제공한다.",
     tools: [skillIcons.figma, skillIcons.illustrator],
     links: [
-      { label: "Figma", url: "https://www.figma.com/proto/U7hpXJPn9bKmb70rGCWWop/planti_app_design?node-id=3-23&viewport=53%2C362%2C0.27&t=qt0GSYraY9BXbBrD-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1" },
+      { label: "Figma", url: "https://www.figma.com/proto/U7hpXJPn9bKmb70rGCWWop/planti_app_design?node-id=1-3&viewport=87%2C192%2C0.3&t=nk9svcbS9UeNZiDu-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1" },
       { label: "Notion", url: "https://app.notion.com/p/App-UI-Planti-3817c519a23080da8b4ef589d38b2975?source=copy_link" },
     ],
   },
